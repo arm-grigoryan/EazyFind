f4f2e3547f26b2a54693057c9949dca3dffe741f
armengrigoryan244 <armengrigoryan244@gmail.com>
2025-10-27 17:22:10 +0400

    Revert "simplify sql init file"
    This reverts commit 55be960aaf3bfe20f849b8640ebc311e1650282d.

--- init-eazyfind.sql
+++ init-eazyfind.sql
@@ -1,3 +1,22 @@
-CREATE DATABASE eazyfind;
-\connect eazyfind;
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_database WHERE datname = 'eazyfind'
+    ) THEN
+        PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE eazyfind');
+    END IF;
+END
+$$;
+
+DO
+$$
+BEGIN
+    PERFORM dblink_exec(
+        'dbname=eazyfind',
+        'CREATE EXTENSION IF NOT EXISTS pg_trgm;'
+    );
+END
+$$;
