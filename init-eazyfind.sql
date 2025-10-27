55be960aaf3bfe20f849b8640ebc311e1650282d
armengrigoryan244 <armengrigoryan244@gmail.com>
2025-10-27 17:14:18 +0400

    simplify sql init file

--- init-eazyfind.sql
+++ init-eazyfind.sql
@@ -1,22 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS dblink;
-
-DO
-$$
-BEGIN
-    IF NOT EXISTS (
-        SELECT FROM pg_database WHERE datname = 'eazyfind'
-    ) THEN
-        PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE eazyfind');
-    END IF;
-END
-$$;
-
-DO
-$$
-BEGIN
-    PERFORM dblink_exec(
-        'dbname=eazyfind',
-        'CREATE EXTENSION IF NOT EXISTS pg_trgm;'
-    );
-END
-$$;
+CREATE DATABASE eazyfind;
+\connect eazyfind;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
